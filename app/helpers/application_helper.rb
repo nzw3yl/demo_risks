7126c0586eb6e661f3f2110bbf648ee17d157749
Roger Smith <roger.smith@ceryx.com>
2010-11-22 14:49:25 -0500

    cleaned up poor commits

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+ def logo
+    image_tag("logo.png", :alt => "Risks & Priorities", :class => "round")
+ end
+
  def title
     base_title = "Risks & Priorities"
     if @title.nil?
