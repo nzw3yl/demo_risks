9a40fa5574e335122676248bde0cc287cff3342b
Roger Smith <roger.smith@ceryx.com>
2010-11-22 22:15:44 -0500

    prior to 10.3

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,6 +9,11 @@ module SessionsHelper
     current_user = nil
   end
 
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+
   def current_user=(user)
     @current_user = user
   end
@@ -17,11 +22,28 @@ module SessionsHelper
     @current_user ||=  user_from_remember_token
   end
 
+  def current_user?(user)
+     user == current_user
+  end
+
   def signed_in?
     !current_user.nil?
   end
 
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+
   private
+
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
+    end
 
     def user_from_remember_token
       User.authenticate_with_salt(*remember_token)
